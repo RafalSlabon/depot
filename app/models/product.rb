8cff0176667359cb6a6894f876112ba3f4ed00ae
Rafal Slabon <slabon@gmail.com>
2012-04-02 22:39:10 +0200

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   validates :title, :uniqueness => true
+  validates_length_of :title, :minimum => 4, :message => "title is too short(<4)"
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :image_url, :format => {
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -4,6 +4,30 @@ class ProductTest < ActiveSupport::TestCase
 
   #fixtures :products
 
+  test "product title is at least 4 characters long" do
+    product = Product.new(
+        :title => "x",
+        :description =>"yyy",
+        :price => "1",
+        :image_url => "fred.gif"
+    )
+
+    assert !product.save
+    assert_equal "title is too short(<4)", product.errors[:title].join(";")
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(
+        :title => products(:ruby).title,
+        :description =>"yyy",
+        :price => "1",
+        :image_url => "fred.gif"
+    )
+
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join(";")
+  end
+
   test "product attributes should not be empty" do
      product = Product.new
      assert product.invalid?
